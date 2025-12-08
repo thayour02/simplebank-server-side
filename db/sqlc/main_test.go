41db2ebf1d1d61e465336960f7d528e3d8fecc0e
thayour02 <you@example.com>
2025-12-08 10:13:50 +0100

    test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -5,24 +5,29 @@ import (
 	"log"
 	"os"
 	"testing"
+
 	_ "github.com/lib/pq"
 )
 
+const dbDriver = "postgres"
 
-var (
-	dbsource = "postgresql://root:secret@localhost:5432/newbank?sslmode=disable"
-	dbDriver = "postgres"
-)
+func getDBSource() string {
+	dbSource := os.Getenv("DB_SOURCE")
+	if dbSource == "" {
+		dbSource = "postgresql://root:secret@localhost:5432/newbank?sslmode=disable"
+	}
+	return dbSource
+}
+
 var testQueries *Queries
 var testDB *sql.DB
 
-func TestMain (m *testing.M) {
+func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbsource)
+	testDB, err = sql.Open(dbDriver, getDBSource())
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 	testQueries = New(testDB)
 	os.Exit(m.Run())
 }
-
